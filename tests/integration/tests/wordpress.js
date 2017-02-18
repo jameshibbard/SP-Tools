b684e581c430388a19014294ac6f6c89247b9333
James Hibbard <james.hibbard@sitepoint.com>
2017-02-18 15:56:16 +0100

    Integration tests

--- tests/integration/tests/wordpress.js
+++ tests/integration/tests/wordpress.js
@@ -15,9 +15,10 @@ const credentials = require('../../../../creds.js');
 // Test Headline Analysis
 // Test Rebuild Link and Copy Link buttons
 // Test [author_more] notification
-// Test that relative links are flagged
+// Test that relative links and empty links are flagged
 // Test that h1 tags are flagged
 // Test that [author_more] and peer review credit in excerpt are flagged
+// Test that [special] shortcode is converted to HTML equivalent
 // Test that incorrect URL is flagged
 // Test ToC generation
 // Test that datepicker works
@@ -70,8 +71,9 @@ module.exports = {
       .assert.hidden('.headalyze-info')
 
       .setValue('#title', ', So it is!')
-      .click('#bandaid-rebuild-link')
       .pause(1000)
+      .click('#bandaid-rebuild-link')
+      .pause(1500)
       .click('.bandaid-copy-link')
       .click('#new-tag-post_tag')
       .keys(browser.Keys.CONTROL)
@@ -89,6 +91,10 @@ module.exports = {
       .pause(2000)
       .assert.containsText('.proofreader-main-row > td', 'Relative link found: www.sitepoint.com')
 
+      .setValue('#content', '\n\n<a href="">Link</a>')
+      .pause(2000)
+      .assert.containsText('.proofreader-main-row > td', 'empty href')
+
       .setValue('#content', '\n\n<h1>What a nice heading!</h1>')
       .pause(2000)
       .assert.containsText('.proofreader-main-row > td', 'H1 tag found!')
@@ -96,6 +102,13 @@ module.exports = {
       .setValue('#excerpt', 'This article was peer reviewed by God [author_more]')
       .pause(2000)
       .assert.containsText('.proofreader-main-row > td', 'Excerpt contains [author_more] shortcode, peer review credit')
+
+      .pause(500)
+      .clearValue('#excerpt')
+
+      .setValue('#excerpt', '[special]I iz speshul![/special]\n\n<p>Awesome content here</p>')
+      .pause(2000)
+      .assert.valueContains('#excerpt', '<p class="wp-special">I iz speshul!</p>\n\n<p>Awesome content here</p>')
 
       .clearValue('#title')
       .setValue('#title', '123456789')
