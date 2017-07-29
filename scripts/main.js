8d3a9b16b28441174efc55191992e1e3222d0ae9
James Hibbard <james.hibbard@sitepoint.com>
2017-07-29 21:03:05 +0200

    Removed newsletters & context menu

--- scripts/main.js
+++ scripts/main.js
@@ -2,23 +2,6 @@
 
 'use strict';
 
-// Responds to context menu options
-//
-chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
-  if (msg.text) {
-    switch (msg.text) {
-      case 'getTitle':
-        sendResponse($('title').text());
-        break;
-      case 'getDesc':
-        sendResponse($('meta[name="description"]').attr('content'));
-        break;
-      default:
-        sendResponse('');
-    }
-  }
-});
-
 const isArticle = $('meta[property="article:section"]').length;
 const isCategory = $('.ChannelBackground').length;
 
@@ -27,25 +10,3 @@ if (isArticle) {
 } else if (isCategory) {
   Channel.init();
 }
-
-// Hack to get actual element a user executed the context menu on
-// This is currently not available natively: https://bugs.chromium.org/p/chromium/issues/detail?id=39507#c14
-// http://stackoverflow.com/questions/7703697/how-to-retrieve-the-element-where-a-contextmenu-has-been-executed
-//
-let clickedEl;
-
-document.addEventListener('contextmenu', (e) => { clickedEl = e.target; }, true);
-
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request === 'getLinkProperties') {
-    if (clickedEl.tagName !== 'A') {
-      clickedEl = clickedEl.closest('a');
-    }
-
-    sendResponse({
-      text: clickedEl.innerHTML.trim(),
-      href: clickedEl.href,
-      title: clickedEl.title,
-    });
-  }
-});
