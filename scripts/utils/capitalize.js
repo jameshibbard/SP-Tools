646a581f14d097512094f862b5e58f86b0fe0225
James Hibbard <1940994+jameshibbard@users.noreply.github.com>
2020-03-05 10:49:40 +0100

    Add tests for capitalize function

--- scripts/utils/capitalize.js
+++ scripts/utils/capitalize.js
@@ -1,168 +1,169 @@
+/* exported capitalize */
 
-var prepositions = [
-    'a',
-    'abaft',
-    'aboard',
-    'about',
-    'above',
-    'absent',
-    'across',
-    'afore',
-    'after',
-    'against',
-    'along',
-    'alongside',
-    'amid',
-    'amidst',
-    'among',
-    'amongst',
-    'an',
-    'apropos',
-    'apud',
-    'around',
-    'as',
-    'aside',
-    'astride',
-    'at',
-    'athwart',
-    'atop',
-    'barring',
-    'before',
-    'behind',
-    'below',
-    'beneath',
-    'beside',
-    'besides',
-    'between',
-    'beyond',
-    'but',
-    'by',
-    'circa',
-    'concerning',
-    'despite',
-    'down',
-    'during',
-    'except',
-    'excluding',
-    'failing',
-    'following',
-    'for',
-    'from',
-    'given',
-    'in',
-    'including',
-    'inside',
-    'into',
-    'lest',
-    'like',
-    'mid',
-    'midst',
-    'minus',
-    'modulo',
-    'near',
-    'next',
-    'notwithstanding',
-    'of',
-    'off',
-    'on',
-    'onto',
-    'opposite',
-    'out',
-    'outside',
-    'over',
-    'pace',
-    'past',
-    'per',
-    'plus',
-    'pro',
-    'qua',
-    'regarding',
-    'round',
-    'sans',
-    'since',
-    'than',
-    'through',
-    'thru',
-    'throughout',
-    'thruout',
-    'till',
-    'times',
-    'to',
-    'toward',
-    'towards',
-    'under',
-    'underneath',
-    'unlike',
-    'until',
-    'unto',
-    'up',
-    'upon',
-    'versus',
-    'vs\.',
-    'vs',
-    'v\.',
-    'v',
-    'via',
-    'vice',
-    'with',
-    'within',
-    'without',
-    'worth'
+const prepositions = [
+  'a',
+  'abaft',
+  'aboard',
+  'about',
+  'above',
+  'absent',
+  'across',
+  'afore',
+  'after',
+  'against',
+  'along',
+  'alongside',
+  'amid',
+  'amidst',
+  'among',
+  'amongst',
+  'an',
+  'apropos',
+  'apud',
+  'around',
+  'as',
+  'aside',
+  'astride',
+  'at',
+  'athwart',
+  'atop',
+  'barring',
+  'before',
+  'behind',
+  'below',
+  'beneath',
+  'beside',
+  'besides',
+  'between',
+  'beyond',
+  'but',
+  'by',
+  'circa',
+  'concerning',
+  'despite',
+  'down',
+  'during',
+  'except',
+  'excluding',
+  'failing',
+  'following',
+  'for',
+  'from',
+  'given',
+  'in',
+  'including',
+  'inside',
+  'into',
+  'lest',
+  'like',
+  'mid',
+  'midst',
+  'minus',
+  'modulo',
+  'near',
+  'next',
+  'notwithstanding',
+  'of',
+  'off',
+  'on',
+  'onto',
+  'opposite',
+  'out',
+  'outside',
+  'over',
+  'pace',
+  'past',
+  'per',
+  'plus',
+  'pro',
+  'qua',
+  'regarding',
+  'round',
+  'sans',
+  'since',
+  'than',
+  'through',
+  'thru',
+  'throughout',
+  'thruout',
+  'till',
+  'times',
+  'to',
+  'toward',
+  'towards',
+  'under',
+  'underneath',
+  'unlike',
+  'until',
+  'unto',
+  'up',
+  'upon',
+  'versus',
+  'vs.',
+  'vs',
+  'v.',
+  'v',
+  'via',
+  'vice',
+  'with',
+  'within',
+  'without',
+  'worth',
 ];
-var articles = [
-    'a',
-    'an',
-    'the'
+
+const articles = [
+  'a',
+  'an',
+  'the',
 ];
-var conjunctions = [
-    'and',
-    'but',
-    'for',
-    'so',
-    'nor',
-    'or',
-    'yet'
+
+const conjunctions = [
+  'and',
+  'but',
+  'for',
+  'so',
+  'nor',
+  'or',
+  'yet',
 ];
-var punct = "([!\"#$%&'()*+,./:;<=>?@[\\\\\\]^_`{|}~-]*)";
-
-var all_lower_case = '(' + (prepositions.concat(articles).concat(conjunctions)).join('|') + ')';
-
-var capitalize = function (title) {
-    var parts = [], split = /[:.;?!] |(?: |^)["Ò]/g, index = 0;
-
-    title = title.replace(/[\u2018\u2019]/g, "'")
-        .replace(/[\u201C\u201D]/g, '"');
-
-    while (true) {
-        var m = split.exec(title);
-
-        parts.push(title.substring(index, m ? m.index : title.length)
-            .replace(/\b([A-Za-z][a-z.'Õ]*)\b/g, function (all) {
-                return /[A-Za-z]\.[A-Za-z]/.test(all) ? all : upper(all);
-            })
-            .replace(RegExp("\\b" + all_lower_case + "\\b", "ig"), lower)
-            .replace(RegExp("^" + punct + all_lower_case + "\\b", "ig"), function (all, punct, word) {
-                return punct + upper(word);
-            })
-            .replace(RegExp("\\b" + all_lower_case + punct + "$", "ig"), upper));
-
-        index = split.lastIndex;
-
-        if (m) parts.push(m[0]);
-        else break;
-    }
-
-    return parts.join("").replace(/ V(s?)\. /ig, " v$1. ")
-        .replace(/(['Õ])S\b/ig, "$1s")
-        .replace(/\b(AT&T|Q&A)\b/ig, function (all) {
-            return all.toUpperCase();
-        });
-};
 
 function lower(word) {
-    return word.toLowerCase();
+  return word.toLowerCase();
 }
 
 function upper(word) {
-    return word.substr(0, 1).toUpperCase() + word.substr(1);
+  return word.substr(0, 1).toUpperCase() + word.substr(1);
+}
+
+const punct = "([!\"#$%&'()*+,./:;<=>?@[\\\\\\]^_`{|}~-]*)";
+
+const allLowerCase = `(${(prepositions.concat(articles).concat(conjunctions)).join('|')})`;
+
+// Lol, WTF is going on in here??
+function capitalize(title) {
+  const parts = []; const split = /[:.;?!] |(?: |^)["Ò]/g;
+  let index = 0;
+
+  const newTitle = title.replace(/[\u2018\u2019]/g, "'")
+    .replace(/[\u201C\u201D]/g, '"');
+
+  /* eslint-disable-next-line no-constant-condition */
+  while (true) {
+    const m = split.exec(newTitle);
+
+    parts.push(newTitle.substring(index, m ? m.index : newTitle.length)
+      .replace(/\b([A-Za-z][a-z.'Õ]*)\b/g, (all) => (/[A-Za-z]\.[A-Za-z]/.test(all) ? all : upper(all)))
+      .replace(RegExp(`\\b${allLowerCase}\\b`, 'ig'), lower)
+      .replace(RegExp(`^${punct}${allLowerCase}\\b`, 'ig'), (all, punctuation, word) => punctuation + upper(word))
+      .replace(RegExp(`\\b${allLowerCase}${punct}$`, 'ig'), upper));
+
+    index = split.lastIndex;
+
+    if (m) parts.push(m[0]);
+    else break;
+  }
+
+  return parts.join('').replace(/ V(s?)\. /ig, ' v$1. ')
+    .replace(/(['Õ])S\b/ig, '$1s')
+    .replace(/\b(AT&T|Q&A)\b/ig, (all) => all.toUpperCase());
 }
--- tests/unit/tests/utilities.test.js
+++ tests/unit/tests/utilities.test.js
@@ -1,9 +1,11 @@
-/* global require, describe, it */
+/* global require, describe, it, capitalize */
+/* eslint-disable max-len */
 
-const assert = require('chai').assert;
-const include = require('../helpers').include;
+const { assert } = require('chai');
+const { include } = require('../helpers');
 
 include('scripts/utils/utilities.js');
+include('scripts/utils/capitalize.js');
 
 const html = `
   <div>
@@ -256,6 +258,32 @@ describe('page object', () => {
       fakeDOM.appendChild(span);
 
       assert.equal(page.getSlug(), '10-reasons-why-js-rocks');
+    });
+  });
+
+  // The capitalize function is a reasonable way to catch obvious errors, but there are a couple of cases it can't handle
+  describe('capitalize function', () => {
+    it('should return a string in title case', () => {
+      // Desired
+      assert.equal(capitalize('this is a heading'), 'This Is a Heading');
+      assert.equal(capitalize('Step 1: Set Up Test Server'), 'Step 1: Set up Test Server');
+      assert.equal(capitalize('Wrapping Up'), 'Wrapping Up');
+      assert.equal(capitalize('15 things zsh can do out of the box'), '15 Things Zsh Can Do out of the Box');
+      assert.equal(capitalize('15 custom aliases to boost your productivity'), '15 Custom Aliases to Boost Your Productivity');
+      assert.equal(capitalize('15 Cool Things You Can Do with (Oh My) ZSH'), '15 Cool Things You Can Do with (Oh My) ZSH');
+      assert.equal(capitalize('Using JavaScript Plugins for Fun And Profit'), 'Using JavaScript Plugins for Fun and Profit');
+      assert.equal(capitalize('10 Time-saving Rails/Rake Aliases'), '10 Time-Saving Rails/Rake Aliases');
+      assert.equal(capitalize('conclusion'), 'Conclusion');
+      assert.equal(capitalize('A Basic HTML5 Template For Any Project'), 'A Basic HTML5 Template for Any Project');
+      assert.equal(capitalize('What is HTML5?'), 'What Is HTML5?');
+      assert.equal(capitalize('The Rest is History'), 'The Rest Is History');
+
+      // Undesired
+      assert.equal(capitalize('Listen for “message” Event'), 'Listen For "Message" Event');
+      assert.equal(capitalize('Step 6: The Problem with the else … if Chain'), 'Step 6: The Problem with the Else … If Chain');
+      assert.equal(capitalize('Step 6: The Problem with the <code>else … if</code> Chain'), 'Step 6: The Problem with the <Code>Else … If</Code> Chain');
+      assert.equal(capitalize('10 Handy npm Aliases'), '10 Handy Npm Aliases');
+      assert.equal(capitalize('The <code>html</code> Element'), 'The <Code>Html</Code> Element');
     });
   });
 });
